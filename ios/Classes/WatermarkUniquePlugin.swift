04b6ef33aaf97df2a0f1bb90c6c37e24a4a3861d
Muhammad Nur Hidayat <momoohidayat@gmail.com>
2025-03-19 16:01:18 +0700

    fix opacity with blend mode

--- ios/Classes/WatermarkUniquePlugin.swift
+++ ios/Classes/WatermarkUniquePlugin.swift
@@ -178,7 +178,7 @@ public class WatermarkUniquePlugin: NSObject, FlutterPlugin {
 
                 if let backgroundColor = backgroundTextColor {
                     backgroundColor.setFill()
-                    UIRectFill(backgroundRect)
+                    UIRectFillUsingBlendMode(backgroundRect, .normal)
                 }
 
                 line.draw(in: textRect, withAttributes: textAttributes)
